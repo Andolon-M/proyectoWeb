9c7eeb09816609bc9485b4e881cd6308703f45c2
Andolon-M <aemendez-l@outlook.com>
2024-05-19 08:23:17 -0500

    feat: :construction: Estructura del diseño lista

---
+++ src/my-navbar.js
@@ -0,0 +1,25 @@
+import { LitElement, css, html } from 'lit'
+
+export class MyNavbar extends LitElement{
+    static properties(){
+           
+    }
+    constructor(){
+        super();
+    }
+
+    render(){
+        
+
+    }
+
+    static get styles(){
+       return css`
+            
+        `
+    }
+
+
+}
+
+customElements.define('my-navbar',MyNavbar);
